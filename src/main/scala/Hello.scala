7d270e107d099001ff7a15175c4f2eac9826c9f4
ozwk <sute.gimei.ozk@gmail.com>
2015-06-24 10:43:41 +0900

    Change main to run easy

--- src/main/scala/Hello.scala
+++ src/main/scala/Hello.scala
@@ -2,22 +2,29 @@ package Hello
 
 import Chisel._
 
-class Hello extends Module {
-  val io = new Bundle { 
+class HelloModule extends Module {
+  val io = new Bundle {
     val out = UInt(OUTPUT, 8)
   }
   io.out := UInt(42)
 }
 
-class HelloTests(c: Hello) extends Tester(c) {
+class HelloTests(c: HelloModule) extends Tester(c) {
   step(1)
   expect(c.io.out, 42)
 }
 
-object Hello {
+object HelloModule {
   def main(args: Array[String]): Unit = {
-    val tutArgs = args.slice(1, args.length)
-    chiselMainTest(tutArgs, () => Module(new Hello())) {
-      c => new HelloTests(c) }
+
+    val args_ = if(args.size > 0 && args(0) == "--test" ){
+      Array("--backend", "c", "--genHarness", "--compile", "--test" ,"--targetDir" , "./cpp")
+    }else{
+      Array("--backend", "v", "--compile", "--targetDir" , "./verilog")
+    }
+
+    HelloTest(args_, () => Module(new HelloModule())) {
+      c => new NiceModuleTest(c)
+          }
   }
 }
